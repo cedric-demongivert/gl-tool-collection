5d78eae1b474407cfa44f35e4e8d8685b8f3d461
Cédric DEMONGIVERT <demc9001@usherbrooke.ca>
2019-10-30 18:10:44 -0400

    build config

--- webpack.mix.js
+++ webpack.mix.js
@@ -8,8 +8,7 @@ for (const name in pckg.dependencies) {
   externals.push(new RegExp(`^${name}(\\/.+)?$`))
 }
 
-mix.ts('src/ts/index.ts', 'dist')
-   .copy('LICENSE.md', 'dist')
+mix.copy('LICENSE.md', 'dist')
    .copy('package.json', 'dist')
    .copy('README.md', 'dist')
    .setPublicPath('dist')
