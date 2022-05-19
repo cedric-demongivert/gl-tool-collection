ac3f6816a75e0a71ecb1bc8799bd05c993216f06
Cédric <cedric.demongivert@gmail.com>
2022-05-19 12:12:31 +0200

    v2 wip

--- src/Clearable.ts
+++
@@ -1,9 +0,0 @@
-/**
- * A clearable object is an object that can be reset to the state in which it was after its instantiation.
- */
-export interface Clearable {
-  /**
-   * Reset this instance to the state it was just after its instantiation.
-   */
-  clear(): void
-}
